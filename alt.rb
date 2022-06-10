d851495f31c8b4d57ac90ee7f1fcedd921fa6c1d
lemur <noctiphobic@gmail.com>
2022-06-10 15:31:16 -0500

    Test that function gives correct result

--- alt.rb
+++ alt.rb
@@ -15,12 +15,19 @@ perms.each do |perm|
 end
 #eliminate index pairs where subsequent number isn't larger
 ordered_stock_pairs = stock_pairs.select {|price_pair, day| day[1] > day[0] } #k,v is price, index. price is arr
-# get their differences?
-#
-# {
-#    [17,3]=>[0,1] //key(prices):value(indexes)
-#    []=>[]
-#    []=>[]
-# }
-p "stock_pairs that are in good order are #{ordered_stock_pairs}"
 
+# get their differences?
+max = 0
+max_index = nil
+max_key = nil
+ordered_stock_pairs.each_with_index do |pair, index|
+  price_pair = pair[0]
+  diff =  price_pair[1] - price_pair[0]
+ if diff > max
+    max = diff
+    max_key = pair[1]
+  else
+    max
+  end
+end
+p "To make $#{max}, buy on day #{max_key[0]} and sell on day #{max_key[1]}"
